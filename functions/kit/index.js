74aeab4d497b742703b5b4d10a59247c637b6c51
only1drhex <lordrhexx@gmail.com>
2023-02-17 16:17:32 +0100

    .

--- functions/kit/index.js
+++ functions/kit/index.js
@@ -1,12 +1,12 @@
 
-var { words } = require("./lib.js")
+var words  = require("./lib.js")
 
 
 export async function onRequestGet(context){
 
 
 
-    return new Response(words.words[0], { headers : {"content-type": "text/html;charset-UTF=8"} });
+    return new Response(words[0], { headers : {"content-type": "text/html;charset-UTF=8"} });
 
 
 }
